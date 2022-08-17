2121a3aa01253b6d8574ce3d3710ea6855820265
danielcaldas <caldasjdaniel@gmail.com>
2022-08-17 17:25:22 +0200

    feat: repro puppeteer/puppeteer#8800 @OrKoN

--- e2e/bootstrap.js
+++ e2e/bootstrap.js
@@ -16,6 +16,7 @@ async function bootstrap(options = {}) {
   await appPage.goto(appUrl, { waitUntil: 'load' });
 
   const targets = await browser.targets();
+  console.info('[TARGETS]: ', targets.map(t => t.type()))
   const extensionTarget = targets.find(target => target.type() === 'service_worker');
   const partialExtensionUrl = extensionTarget.url() || '';
   const [, , extensionId] = partialExtensionUrl.split('/');
